4c1f9be19cc6d07333194481eb332fca99674de2
Gerardo Bort <gerardobort@gmail.com>
2013-06-14 16:32:49 -0700

    updates in model.js

--- views/js/require-model.js
+++ views/js/require-model.js
@@ -4,19 +4,7 @@ define('model/<%= collection.name %>', [
         <%= _(collection.relations).filter(function (relation) { return 'fs.files' !== relation.relatedCollection; }).map(function (relation, key) { return relation.relatedCollection.toCamelCase(); }).join(', ') %>
     ) {
     <%
-
         var schema = {};
-
-        if ((s.type === 'List' && s.itemType === 'ObjectId') || (s.type === 'ObjectId')) {
-            var relatedCol = _(global.config.collections).find(function (c) {
-                return c.name === collection.relations[key].relatedCollection;
-            });
-            schema[key].help = 'Search for ' + relatedCol.humanName 
-                + ' > ' + (relatedCol.backboneForms.schema[relatedCol.toStringField].title||relatedCol.toStringField);
-            schema[key].autocompleteField = relatedCol.toStringField;
-            schema[key].autocompleteCollectionName = relatedCol.name;
-        }
-
     %>
 
     Backbone.Model.<%= collection.name.toCamelCase() %> = Backbone.DeepModel.extend({
