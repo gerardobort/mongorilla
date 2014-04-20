fe19e0a9e989df530f293ab80c5c4747760bad5a
Gerardo Bort <gerardobort@gmail.com>
2014-04-20 03:48:41 -0300

    adding extensibility to forms, first experimental approach, but seems so good

---
+++ public/forms/mongorillaRole.js
@@ -0,0 +1,32 @@
+define('forms/mongorillaRole', [
+    'forms/base/mongorillaRole',
+    'models/mongorillaRole'
+    ], function (MongorillaRoleFormBase, MongorillaRoleModel) {
+
+    Backbone.Form.MongorillaRole = MongorillaRoleFormBase.extend({
+
+        events: {
+        },
+
+        initialize: function () {
+            var instance = this;
+
+            setTimeout(function () {
+                var warning = '\
+                    <div class="alert alert-warning alert-dismissable diff-report">\
+                        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>\
+                        <h3><strong>Warning!</strong> <code>Mongorilla Role</code> is currently an experimental feature.</h3>\
+                        <p>We don\'t guarantee future support yet, since this is now subject to testing and approval.</p>\
+                    </div>\
+                ';
+                $('#main-container').prepend(warning);
+            }, 0);
+            return Backbone.Form.prototype.initialize.apply(this, arguments);
+        },
+
+
+    });
+
+    return Backbone.Form.MongorillaRole;
+
+});
