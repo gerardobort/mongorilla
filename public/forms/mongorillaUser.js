2ed27965303791ed2902d6fe45a48ea2bb82bee5
Gerardo Bort <gerardobort@gmail.com>
2014-04-20 03:56:59 -0300

    adding extensibility to forms, first experimental approach, but seems so good

--- public/forms/mongorillaUser.js
+++ public/forms/mongorillaUser.js
@@ -13,12 +13,13 @@ define('forms/mongorillaUser', [
 
             setTimeout(function () {
                 var warning = '\
-                    <div class="alert alert-warning alert-dismissable diff-report">\
+                    <div class="alert alert-warning alert-dismissable experimental-warn">\
                         <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>\
                         <h3><strong>Warning!</strong> <code>Mongorilla User</code> is currently an experimental feature.</h3>\
                         <p>We don\'t guarantee future support yet, since this is now subject to testing and approval.</p>\
                     </div>\
                 ';
+                $('.experimental-warn').remove();
                 $('#main-container').prepend(warning);
             }, 0);
             return Backbone.Form.prototype.initialize.apply(this, arguments);
