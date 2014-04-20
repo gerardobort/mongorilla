7c460381ba20ddfb0f9de5d9e36f97352806c16d
Gerardo Bort <gerardobort@gmail.com>
2014-04-20 03:43:36 -0300

    adding extensibility to forms, first experimental approach, but seems so good

--- public/forms/mongorillaCollection.js
+++ public/forms/mongorillaCollection.js
@@ -10,6 +10,22 @@ define('forms/mongorillaCollection', [
             'click #validators li input': 'toggleValidatorsConditionalFields',
         },
 
+        initialize: function () {
+            var instance = this;
+
+            setTimeout(function () {
+                var warning = '\
+                    <div class="alert alert-warning alert-dismissable diff-report">\
+                        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>\
+                        <h3><strong>Warning!</strong> <code>Mongorilla Collection</code> is currently an experimental feature.</h3>\
+                        <p>We don\'t guarantee future support yet, since this is now subject to testing and approval.</p>\
+                    </div>\
+                ';
+                $('#main-container').prepend(warning);
+            }, 0);
+            return Backbone.Form.prototype.initialize.apply(this, arguments);
+        },
+
         toggleTypeConditionalFields: function (event) {
             var $type = $(event.target),
                 editorType = $type.val();
