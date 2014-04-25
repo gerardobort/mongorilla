549c90bf850ab927f89091f7f47e90351a776820
Gerardo Bort <gerardobort@gmail.com>
2014-04-24 23:47:11 -0300

    going to sleep

--- routes/api/generic.js
+++ routes/api/generic.js
@@ -123,7 +123,8 @@ exports.put = function (req, res) {
     delete attributes['_id'];
 
     // TODO skip all attributes not specified in schema
-    var attributesToSet = global.helpers.toFlat(attributes);
+    //var attributesToSet = global.helpers.toFlat(attributes);
+    var attributesToSet = global.helpers.deepClone(attributes);
     attributesToSet[collection.updatedField.key] = new global[collection.createdField.type||'Date']().toISOString();
 
 
